73658f86d0082d622a8cabb11574bb95c8c4f222
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-08-24 10:12:59 +0700

    setup

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCaIqVeAq7Yjc72FRkmsC8f__WLBaSYfJs",
+    authDomain: "netflix-f4c8c.firebaseapp.com",
+    projectId: "netflix-f4c8c",
+    storageBucket: "netflix-f4c8c.appspot.com",
+    messagingSenderId: "417617488826",
+    appId: "1:417617488826:web:056f2619e05c91aff14b83"
+  };
